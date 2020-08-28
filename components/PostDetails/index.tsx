fd1666a32bb6df47a653a9665abf3645500b0345
Cleberson F R Junior <cleberson.dev@gmail.com>
2020-08-28 20:53:42 -0300

    Removed commented code on PostDetails component

--- components/PostDetails/index.tsx
+++ components/PostDetails/index.tsx
@@ -23,45 +23,6 @@ const PostDetails: React.FC<Props> = ({ title, category, cover, content, created
           <img src="/calendar.svg" alt="Ícone de data" /> {format(new Date(createdAtMs), "dd 'de' LLLL 'de' yyyy", { locale: ptBR })}
         </S.Date>
         <S.Content dangerouslySetInnerHTML={{ __html: content }} />
-        {/* <S.Content>
-          <p>
-            Você já ouviu falar sobre{" "}
-            <a href="https://www.typescriptlang.org/">TypeScript</a>? Trata-se
-            de uma ferramenta que permite escrever um código, fazendo
-            programações orientadas a objetos sem perder suas vantagens. Ao
-            compilar um código TypeScript, é gerado um código JavaScript, e esse
-            código é o que será executado no browser (navegador).
-          </p>
-          <h2>O que é TypeScript?</h2>
-          <p>
-            Entre os principais elementos básicos que tornam essa construção
-            muito importante, podemos citar templates, componentes, roteamento,
-            diretivas, módulos, serviços, injeção de dependências e ferramentas
-            de infraestrutura. Se você quer entender um pouco mais sobre esse
-            tema, continue a leitura e saiba mais!
-          </p>
-          <h3>Um sub-título da hora 😂</h3>
-          <p>
-            Entre os principais elementos básicos que tornam essa construção
-            muito importante, podemos citar templates, componentes, roteamento,
-            diretivas, módulos, serviços, injeção de dependências e ferramentas
-            de infraestrutura. Se você quer entender um pouco mais sobre esse
-            tema, continue a leitura e saiba mais!
-          </p>
-          <figure>
-            <img
-              src="https://unsplash.com/photos/m_HRfLhgABo/download?force=true&w=640"
-              alt="Ilustração"
-            />
-            <figcaption>Snippet de código escrito em Javascript</figcaption>
-          </figure>
-          <p>
-            Além do TypeScript, existem outras formas de tipagem de código. No
-            React, por exemplo, é possível realizar a tipagem de propriedades de
-            componentes utilizando o PropTypes, um tipo de checagem de tipagem
-            dinâmica que funciona em tempo de execução.
-          </p>
-        </S.Content> */}
       </Wrapper>
     </S.Container>
   );
